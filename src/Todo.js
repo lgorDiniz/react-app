c7772e0cf7dd8e0fb3390f688f7cf4945f2e0fba
lgorDiniz <igordinizsilveira@hotmail.com>
2025-10-15 23:50:10 -0400

    Created React Project
    Started new Todo-list React Project.

---
+++ src/Todo.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import "./Todo.css";
+
+function Todo(){
+
+    const [text, setText] = useState("");
+    
+    function handleChange(event){
+        let t = event.target.value;
+        setText(t);
+    }
+    
+    return (
+        <div className="container">
+            <h1>Todo</h1>
+            <form>
+                <input onChange={handleChange} type="text"></input>
+                <button>Add</button>
+            </form>
+
+            <ul>
+                <li>{text}</li>
+            </ul>
+        </div>)
+}
+
+export default Todo;
